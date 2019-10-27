fc0c8ef835acd43805eea9f30d3a4c4e8edeef87
davidrmur <davidrmurga@gmail.com>
2019-10-27 16:20:24 -0400

    removing import

--- gui.py
+++ gui.py
@@ -2,8 +2,6 @@ import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
 import openpyxl
 import os
-import time
-
 
 # opening the existing excel file
 filename = 'userdata.xlsx'
